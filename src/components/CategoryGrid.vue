86302dca0269b674ea7bafcaa7d2c7dc4c659359
BuildTools <unconfigured@null.spigotmc.org>
2019-03-03 21:46:15 +0800

    split into components

---
+++ src/components/CategoryGrid.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "CategoryGrid"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/store/appStore.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "appStore"
+    }
+</script>
+
+<style scoped>
+
+</style>
